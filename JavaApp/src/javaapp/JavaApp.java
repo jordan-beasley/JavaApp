3ff3e1026f33ac2804e0b0b5b9654e29169a3ff4
calebprice22 <36460560+calebprice22@users.noreply.github.com>
2018-04-23 16:53:09 -0500

    icon update
    sets icon

--- JavaApp/src/javaapp/JavaApp.java
+++ JavaApp/src/javaapp/JavaApp.java
@@ -1,10 +1,16 @@
 package javaapp;
 
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javax.imageio.ImageIO;
 
 
 public class JavaApp extends Application {
@@ -15,7 +21,11 @@ public class JavaApp extends Application {
         
         Scene scene = new Scene(root);
         
+        BufferedImage bufferedImg = ImageIO.read(new File("C:\\Users\\caleb\\Pictures\\JavaApp.png"));;
+        Image i = SwingFXUtils.toFXImage(bufferedImg, null);
+
         stage.setTitle("OpenEdit");
+        stage.getIcons().add(i);
         stage.setScene(scene);
         stage.show();
     }
